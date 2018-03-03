601d5d8c70e4ca0b0e2d59ba1453ac87df9f8ae2
holden2000 <holden2000@mac.com>
2018-03-03 14:48:21 -0500

    loading files

---
+++ Maps/Scripts/Maps.js
@@ -0,0 +1,5 @@
+var stateList = ["HI", "AK", "FL", "SC", "GA", "AL", "NC", "TN", "RI", "CT", "MA",
+                 "ME", "NH", "VT", "NY", "NJ", "PA", "DE", "MD", "WV", "KY", "OH",
+                 "MI", "WY", "MT", "ID", "WA", "DC", "TX", "CA", "AZ", "NV", "UT",
+                 "CO", "NM", "OR", "ND", "SD", "NE", "IA", "MS", "IN", "IL", "MN",
+                 "WI", "MO", "AR", "OK", "KS", "LS", "VA"];
